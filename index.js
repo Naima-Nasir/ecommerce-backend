304bf8e15f671aa45b7e2e586c54117049961ab0
Naima-Nasir <naimanasir114@gmail.com>
2026-07-29 21:51:54 +0500

    index.js

--- index.js
+++ index.js
@@ -15,17 +15,18 @@ app.use(express.json());
 // 🔌 MYSQL CONNECTION
 // =====================
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "ecommerce",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   charset: "utf8mb4"
 });
 
 db.connect((err) => {
   if (err) {
     console.log("❌ DB connection failed", err);
-     console.log(err);
+    console.log(err);
   } else {
     console.log("✅ Connected to MySQL");
   }
