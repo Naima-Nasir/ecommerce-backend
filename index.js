ef146212dec6cf394bd026d10206cc2a1ce9f57b
Naima-Nasir <naimanasir114@gmail.com>
2026-07-30 14:40:25 +0500

    index.js

--- index.js
+++ index.js
@@ -440,13 +440,18 @@ app.post("/forgot-password", (req, res) => {
             });
           }
 
-          const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-              user: process.env.EMAIL_USER,
-              pass: process.env.EMAIL_PASS,
-            },
-          });
+        const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
           try {
 
