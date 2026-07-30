6fa5ecb99af137ea6103f2b423fef91c405e9e47
Naima-Nasir <naimanasir114@gmail.com>
2026-07-30 14:55:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -439,13 +439,15 @@ app.post("/forgot-password", (req, res) => {
               message: "Failed to save OTP",
             });
           }
- const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-              user: process.env.EMAIL_USER,
-              pass: process.env.EMAIL_PASS,
-            },
-          });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
           try {
 
